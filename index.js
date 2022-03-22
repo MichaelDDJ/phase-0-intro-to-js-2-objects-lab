ca53dbf9c5b558c3fc1b9f7545a37450b9219312
Michael Davis <jrmdd1496@gmail.com>
2022-03-22 12:39:29 -0500

    commit tree 7a57d1253647974b07288906f9656e32e912ed16

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {
+    name: "Michael Davis",
+    streetAddress: "1000 Something St.",
+}
+
+function updateEmployeeWithKeyAndValue(object, key, value) {
+    const newObj = {...object};
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue (object, key, value) {
+    object[key] = value;
+    return object;
+}
+
+function deleteFromEmployeeByKey (object, key) {
+    let newObj = {...object};
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey (object, key) {
+    delete object[key];
+    return object;
+}
